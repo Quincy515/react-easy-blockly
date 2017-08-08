9a496f28666c48081bdcedcaafe66bb082805a68
custertian <custertian@users.noreply.github.com>
2017-08-08 15:38:19 +0800

    https://github.com/melbings/ariadne
    https://github.com/melbings/ariadne
    实现了基本功能但是有问题

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
-import BlocklyDiv from './BlocklyDiv'
+import BlocklyContainer from './BlocklyDiv'
 
-
+const _TOOLBOX = `
+    <xml>
+    <block type="controls_if"></block>
+    <block type="logic_compare"></block>
+    </xml>
+    `
 class App extends Component {
   render() {
     return (
       <div>
         <h1>container is not in current document</h1>
-        <BlocklyDiv/> 
+        <BlocklyContainer
+        ref="blocklyComponent"
+        toolbox={_TOOLBOX}
+        /> 
       </div>
     );
   }
