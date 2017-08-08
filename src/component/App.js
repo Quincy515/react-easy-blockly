05e6f5be09ef3f4c6aa24ad62db88327cbe60744
custertian <custertian@users.noreply.github.com>
2017-08-08 15:50:14 +0800

    简单版本
    import Blockly from 'node-blockly/browser';

--- src/component/App.js
+++ src/component/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
-import BlocklyContainer from './BlocklyDiv'
+import BlocklyDiv from './BlocklyDiv'
+import '../css/App.css'
 
-const _TOOLBOX = `
-    <xml>
-    <block type="controls_if"></block>
-    <block type="logic_compare"></block>
-    </xml>
-    `
 class App extends Component {
   render() {
     return (
       <div>
         <h1>container is not in current document</h1>
-        <BlocklyContainer
-        ref="blocklyComponent"
-        toolbox={_TOOLBOX}
-        /> 
+        <BlocklyDiv/> 
       </div>
     );
   }
--- src/component/BlocklyDiv.js
+++ src/component/BlocklyDiv.js
@@ -1,118 +1,34 @@
-import $ from 'jquery';
+
 import React from 'react';
-import ReactDOM from 'react-dom'
 import Blockly from 'node-blockly/browser';
+const toolbox = `
+         <xml>
+           <block type="controls_if"></block>
+           <block type="controls_whileUntil"></block>
+         </xml>`
 
-export default React.createClass({
-
-    addReservedWordsToBlockly(words) {
-        Blockly.JavaScript.RESERVED_WORDS_ = this._originalReservedWords;
-        Blockly.JavaScript.addReservedWords(words);
-    },
-
-    clear() {
-        this._workspace.clear();
-    },
-
-    getCode: function (statementPrefix) {
-        Blockly.JavaScript.STATEMENT_PREFIX = statementPrefix;
-        return Blockly.JavaScript.workspaceToCode(this._workspace);
-    },
-
-    getXmlText: function () {
-        return Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(this._workspace));
-    },
-
-    highlightBlock: function (id) {
-        this._workspace.traceOn(true);
-        this._workspace.highlightBlock(id);
-    },
-
-    loadXmlText: function (xmlText) {
-        this._workspace.clear();
-        Blockly.Xml.domToWorkspace(this._workspace, Blockly.Xml.textToDom(xmlText));
-    },
-
-    setReadOnly: function (readOnly) {
-        this._workspace.options.readOnly = readOnly;
-        if (readOnly) {
-            $('.blocklyToolboxDiv').hide();
-        } else {
-            $('.blocklyToolboxDiv').show();
+class BlocklyDiv extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            workspace: null
         }
-    },
-
-    // REACT CODE BELOW
-
-    propTypes: {
-        toolbox: React.PropTypes.string.isRequired,
-        customBlocks: React.PropTypes.object
-    },
-
-    componentDidMount: function () {
-        this._blocklyDiv = ReactDOM.findDOMNode(this);
-        this._blocklyArea = this._blocklyDiv.parentElement;
-
-        $(() => {
-            if (this.props.customBlocks) this.props.customBlocks.loadBlocksIntoBlockly(Blockly);
-
-            this._workspace = Blockly.inject(this._blocklyDiv,
-                {
-                    grid: {
-                        spacing: 25,
-                        length: 3,
-                        colour: '#ccc',
-                        snap: false
-                    },
-                    scrollbars: true,
-                    media: './blockly/blockly-media/',
-                    toolbox: this.props.toolbox,
-                    trashcan: true,
-                    zoom: {
-                        enabled: true
-                    }
-                });
-            this._originalReservedWords = Blockly.JavaScript.RESERVED_WORDS_;
-
-            window.addEventListener('resize', this._onWindowResize, false);
-            this._onWindowResize();
-        });
-    },
-
-    // Re-rendering Blockly is not supported because initialization of toolbox and workspace happen only once.
-    shouldComponentUpdate: function (nextProps, nextState) {
-        return false;
-    },
-
-    render: function () {
-        return (
-            <div className="blockly-component">
-                {this.props.children}
-            </div>
-        );
-    },
-
-    componentWillUnmount: function () {
-        window.removeEventListener('resize', this._onWindowResize, false);
-    },
-
-    _onWindowResize: function (event) {
-        // Compute the absolute coordinates and dimensions of blocklyArea.
-        let element = this._blocklyArea;
-        let x = 0;
-        let y = 0;
-
-        do {
-            x += element.offsetLeft;
-            y += element.offsetTop;
-            element = element.offsetParent;
-        } while (element);
-
-        // Position blocklyDiv over blocklyArea.
-        this._blocklyDiv.style.left = x + 'px';
-        this._blocklyDiv.style.top = y + 'px';
-        this._blocklyDiv.style.width = this._blocklyArea.offsetWidth + 'px';
-        this._blocklyDiv.style.height = this._blocklyArea.offsetHeight + 'px';
     }
+    componentDidMount() {
+        this.setState({
+            workspace: Blockly.inject(this.blocklyDiv, { toolbox: toolbox })
+        })
 
-});
+    }
+    render() {
+        return (
+            <div>
+                <h2 >BlocklyDiv</h2>
+                <div id="blocklyContainer">
+                    <div id="blocklyDiv" ref={ref => this.blocklyDiv = ref} />
+                </div>
+            </div>
+        )
+    }
+}
+export default BlocklyDiv
